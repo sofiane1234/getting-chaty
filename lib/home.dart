617d3cb12258aec32a3159601a06ca29e4c0035c
sofiane1234 <79451497+sofiane1234@users.noreply.github.com>
2022-04-19 16:42:52 +0200

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,35 +1,81 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
-
 import 'login.dart';
-
+var loginUser = FirebaseAuth.instance.currentUser;
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
+  final auth = FirebaseAuth.instance;
+  getCurrentUser() {
+    final user = auth.currentUser;
+    if(user != null) {
+      loginUser = user;
+    }
+  }
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  final storeMsg = FirebaseFirestore.instance;
+
+  TextEditingController msg = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Geting Chaty"),
+        title: Text(loginUser!.email.toString()),
+        elevation: 0,
         actions: [
           IconButton(onPressed: () async => {
             await FirebaseAuth.instance.signOut(),
           Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => Login()))},
-              icon: Icon(CupertinoIcons.back,
-              color: Colors.deepPurpleAccent,)),
-
+              icon: Icon(Icons.logout,
+              color: Colors.cyanAccent,)
+          ),
         ],
       ),
-      body: Center(
-        child: Text('HomePage'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text("Discussions", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),),
+          Row(children: [
+            Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      top: BorderSide(
+                        color: Colors.green,
+                        width: 2.5,
+                      ),
+                    ),
+                  ),
+              child: TextField(
+                controller: msg,
+                decoration: InputDecoration(
+                  hintText: "Ecrire un message...",
+                  hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            )
+            ),
+            IconButton(onPressed: (){
+              if (msg.text.isNotEmpty) {
+                storeMsg.collection("Messages").doc().set({
+                  "messages":msg.text.trim(),
+                  "user":loginUser!.email.toString(),
+                });
+                msg.clear();
+              }
+
+            }, icon: Icon(Icons.send,
+              color: Colors.redAccent,),)
+          ],
+          ),
+        ],
       ),
     );
   }
