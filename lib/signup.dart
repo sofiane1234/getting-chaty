686cd8d12282d8c54f4894b30aeaf8a61fc07413
sofiane1234 <79451497+sofiane1234@users.noreply.github.com>
2022-04-20 16:31:15 +0200

    Update signup.dart

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,7 +1,8 @@
+import 'package:chatyap/firebaseHelper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'login.dart';
 
 
 class SignUp extends StatefulWidget {
@@ -13,58 +14,66 @@ class SignUp extends StatefulWidget {
 
 class _SignUpState extends State<SignUp> {
   final emailController = TextEditingController();
-  final pseudoController = TextEditingController();
   final passwordController = TextEditingController();
+  Service service = Service();
+
   @override
   Widget build(BuildContext context) {
     User? user = FirebaseAuth.instance.currentUser;
     return Scaffold(
-      appBar: AppBar(title: Text('Getting Chaty (Logged ' + (user == null ? 'out)':'in)'),
-        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),),
+      appBar: AppBar(title: Text('Getting Chaty Inscription',
+        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),),
       backgroundColor: Colors.cyanAccent,
-      body:  Container(
+      body: Container(
         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
         child: Column(
           children: [
             TextFormField(
               decoration: InputDecoration(
-                hintText: 'Addresse Email',
-                hintStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,)
+                  hintText: 'Addresse Email',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)
+                  ),
+                  hintStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)
               ),
               controller: emailController,
               style: TextStyle(fontSize: 19),
             ),
-
-            TextFormField(
+            Padding(padding: EdgeInsets.symmetric(vertical: 5)),
+            TextFormField(controller: passwordController,
               decoration: InputDecoration(
-                hintText: 'Pseudo',
-                hintStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,)
+                  hintText: 'Mot de passe',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)
+                  ),
+                  hintStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,)
               ),
-              controller: pseudoController,
-              style: TextStyle(fontSize: 19),
-            ),
-
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Mot de passe',
-                hintStyle: TextStyle(
-                  color: Colors.black87,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              controller: passwordController,
               obscureText: true,
               style: TextStyle(fontSize: 19),
             ),
+
+            Container(
+              alignment: Alignment.centerRight,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 15),
+                child: TextButton(onPressed: (){
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: ((context) => Login())));
+                }, child: Text('Se connecter ici !'),),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 10),
+            ),
+
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(child: Text('SignUn'),
-                  onPressed: () async{
-                    await FirebaseAuth.instance.createUserWithEmailAndPassword(email: emailController.text,
-                        password: passwordController.text);
-                    setState(() {});
-                  }, ),
+                ElevatedButton(onPressed: () {
+                  if(emailController.text.isNotEmpty && passwordController.text.isNotEmpty) {
+                    service.createAccount(context, emailController.text, passwordController.text);
+                  } else {
+                    service.error(context, "Veuillez remplir tous les champs !");
+                  }
+                },
+                    child: Text('Inscription')),
               ],
             )
           ],
